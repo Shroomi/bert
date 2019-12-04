676f7d03d0686080607e308a9516752616136271
丁梦茹 <dingmengru1993@gmail.com>
2019-12-04 11:34:41 +0800

    存储一些配置信息

---
+++ config.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+"""
+@author: Ding Mengru
+@contact: dingmengru1993@gmail.com
+
+@version: 1.0
+@file: config.py
+@time: 03.12.19 11:43
+
+存储一些配置信息
+"""
+
+
+class Config(object):
+    # 数据库配置
+    TXT_CLEAN_SQL = 'mysql+pymysql://root:shannon@172.22.22.141:7788/txt_clean?charset=utf8'

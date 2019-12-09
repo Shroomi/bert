4837e743d9cba7a2d7bd3a3c61313824197449ba
丁梦茹 <dingmengru1993@gmail.com>
2019-12-09 10:42:27 +0800

    添加txt和tfrecord路径

--- bert/run_create_pretraining_data.sh
+++ bert/run_create_pretraining_data.sh
@@ -1,10 +1,11 @@
 # 将/data/dmr/one_sent_per_line下的数据转换成tf_record数据类型，存放在/data/dmr/tf_record下
 
 export BERT_BASE_DIR=/home/dingmengru/workspace/bert
+export DATA_DIR=/data/dmr
 
 python create_pretraining_data.py \
-	--input_file=$BERT_BASE_DIR/data/sample_text_chinese.txt \
-	--output_file=$BERT_BASE_DIR/tmp/tf_examples.tfrecord \
+	--input_file=$DATA_DIR/one_sent_per_line/38389331.txt \
+	--output_file=$DATA_DIR/tf_record/38389331.tfrecord \
 	--vocab_file=$BERT_BASE_DIR/chinese_L-12_H-768_A-12/vocab.txt \
 	--max_seq_length=512 \
 	--max_predictions_per_seq=77 \
